7c13d3b43efc73075ffe8382ae1cd1a76bf802c1
Roman Yudin <hellfrom@mail.ru>
2020-04-19 22:08:18 +0300

    chore(jest.config.js): update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  // collectCoverageFrom: ['**/*.js', '!**/_*/**', '!**/_*.js', '!**/__stories__/**'],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: undefined,
@@ -138,9 +138,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/generators'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   testRegex: ['(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$'],
