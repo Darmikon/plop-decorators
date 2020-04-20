a3a542b1dfdaf38bbb4275b46db1cdcaa462bd37
Roman Yudin <hellfrom@mail.ru>
2020-04-20 16:12:16 +0300

    chore(package.json): add standard-version to scripts section

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // https://github.com/conventional-changelog/standard-version#lifecycle-scripts
+  scripts: {
+    prebump: 'echo Bumping version starts',
+    // postchangelog:
+    //   "replace 'https://github.com/myproject/issues/' 'https://myjira/browse/' CHANGELOG.md",
+  },
+  // skip: {
+  //   bump: true,
+  //   changelog: true,
+  //   commit: true,
+  //   tag: true,
+  // },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+![GitHub package.json version](https://img.shields.io/github/package-json/v/darmikon/plop-decorators) ![npm](https://img.shields.io/npm/v/plop-decorators) [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
