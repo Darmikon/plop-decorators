974706e389881658ccce773b6d4f8559e85fb829
Roman Yudin <hellfrom@mail.ru>
2020-05-03 13:39:09 +0300

    chore(lint-staged.config.js): add 'git add' command at the end

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,7 @@ module.exports = {
     'prettier --write',
     'eslint --fix',
     'jest --bail --findRelatedTests',
+    'git add',
   ],
-  // 'src/**/*.scss': ['stylelint --syntax scss --fix'],
+  // 'src/**/*.scss': ['stylelint --syntax scss --fix', 'git add'],
 };
